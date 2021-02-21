c505bb2cae09046513342a457ef09641525612a4
Alexandra O'Neil <aconeil@iu.edu>
2021-02-21 17:03:18 -0500

    firstPractical

--- 00_Corpus/segmenter.py
+++ 00_Corpus/segmenter.py
@@ -1,4 +1,5 @@
 import sys
+import re
 
 line = sys.stdin.readline()
 
@@ -8,7 +9,6 @@ while line != '':
 	for token in line.split(' '):
 		#if the token is new line
 		if token == '\n':
-			#print the new line
 			print(token)
 		#strip all tokens of newline
 		token = token.strip()
@@ -19,17 +19,33 @@ while line != '':
 		#if the last character of the sentence is ! or ? add newline after it
 		if token[-1] in '!?':
 			sys.stdout.write(token + '\n')
-		#if the last character of the period is a period
+		#if the last code of the token is a quote mark
+		if token[-1] in '"':
+			if re.findall('\."', token):
+				sys.stdout.write(token + '\n')
+			else:
+				sys.stdout.write(token + ' ')
+		#if the last character of the token is a period
 		elif token[-1] in '.':
 #			if the token is in the list
 			if token in ['Dr.','etc.','i.e.', 'e.g.', 'St.', '...']:
+				sys.stdout.write(token + ' ')
+			#if the token uses a period following one or more numbers
+			elif re.findall('[0-9]+\.[0-9]+\.', token):
 			#write out the token with a space following it
 				sys.stdout.write(token + ' ')
-			#add new line to all tokens following a period
-			#otherwise write it our with newline following it
+			#if the token is a capital letter followed by a period
+	#		elif re.findall('[A-Z]+\.',token):
+	#			sys.stdout.write(token + '\n')
+			elif re.findall('[A-Z]\.',token):
+				if re.findall('[A-Z][A-Z]+\.', token):
+					sys.stdout.write(token + '\n')
+				else:
+					sys.stdout.write(token + ' ')
+		#otherwise write it out with newline following it
 			else:
 				sys.stdout.write(token + '\n')
-		#print out the tokens with space between if last charcter in token is not !?.
+	#print out the tokens with space between if last charcter in token is not !?.
 		else:
 			sys.stdout.write(token + ' ')
 	line = sys.stdin.readline()
