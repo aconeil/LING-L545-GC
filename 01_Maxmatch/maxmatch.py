25a543e2594a57a405ca005ef9fe6ae7f9f79043
Alexandra O'Neil <aconeil@iu.edu>
2021-02-26 14:50:46 -0500

    2.26.2021

--- 01_Maxmatch/maxmatch.py
+++ 01_Maxmatch/maxmatch.py
@@ -1,35 +1,37 @@
 import sys
-#dictionary.sort()
-#dictionary.reverse()
 
 def maxmatch(sentence, dictionary):
         #dictionary = a dictionary containing your vocabulary
-        #return dictionary value for sentence
+        #if sentence is not empty return a list
         if sentence == '':
                 return []
-        #for variable i in the range from the start of the sentence to the end of the sentence
+        #for a variable in the range from the start of the sentence to the maximum length of the sentence
         for i in range(0, len(sentence)):
-                #the first word is the word located between 0 and -i
+                #the first word is the word located between the start of the sentence and the value i from the end of the sentence
                 firstword = sentence[0:-i]
+#               print(firstword)
                 # the remainder is everything after
                 remainder = sentence[-i:]
                 #if there is a match between the first word and a dictionary entry
                 if firstword in dictionary:
-                        #return the first word and the maxmatch of the rest of the sentence and the dictionary
+                       #return the first word and the maxmatch of the rest of the sentence and the dictionary
                         return [firstword] + maxmatch(remainder, dictionary)
-        #Otherwise the first word is located at 0
+                #Otherwise the first word is the first character
         firstword = sentence[0]
-        #and the remainder is everything after 1
+                #and the remainder is one and everything after it
         remainder = sentence[1:]
-        #return the first word plus the remainder's maxmatch
-        return ([firstword] + maxmatch(remainder, dictionary))
+                #return the first word plus the maxmatch for the remainder
+        return [firstword] + maxmatch(remainder, dictionary)
 
-dictionary = [line.strip() for line in open("extracted_japanese.txt").readlines()]
+#the dictionary is a list of the text in the extracted japanese text file that is stripped of newline
+dictionary = [line.strip() for line in open("ja-extracted.txt").readlines()]
+#open the file from the file on the command line:
 fd = open(sys.argv[1])
 line = fd.readline()
+
+#line = sys.stdin.readline()
+#while the line is not an empty string
 while line != '':
         #print out the maxmatch tokenised line
-        print(maxmatch(dictionary, line.strip('\n')))
-        #read a line from the file
+        print(' '.join(maxmatch(line.strip('\n'),dictionary)))
         line = fd.readline()
-#Some error with lists? or possibly I need a dict file?
